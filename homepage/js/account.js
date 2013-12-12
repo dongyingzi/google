7fbb6da15dbfdd87146ad437237fe35a76e87b16
qiu deqing <deqing.qiu@gmail.com>
2013-12-12 23:14:34 +0800

    dommy

--- homepage/js/account.js
+++ homepage/js/account.js
@@ -3,6 +3,52 @@
 * @class Account
 * @constructor
 **/
-function Account(container) {
+function Account(account) {
   var that = this;
+  this.account = dome.select(account);
+  this.itemts = this.account.selectAll(".account__item");
+  this.links = this.account.selectAll(".account__link");
+  this.itemToggle = this.account.selectAll(".account__item-toggle");
+  this.itemToggle.addEventListener("click", function (event) {
+    Account.toggleItemClassFromChild(event.target, "account__item--active");
+  }, false);
 } // end Account()
+
+/**
+* 为元素父.account__item切换cls
+* @method toggleItemClassFromChild
+* @param element {Element} 需要为父.account__item切换cls的对象
+* @param cls {string} 需要切换的class
+**/
+Account.toggleItemClassFromChild = function (element, cls) {
+  var p = element.parentNode;
+  while (p !== null && !dome.hasClass(p, "account__item")) {
+    p = p.parentNode;
+  } // end while
+  if (p !== null) {
+    dome.toggleClass(p, cls);
+  } // end if
+  
+} // end toggleItemClassFromChild()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
