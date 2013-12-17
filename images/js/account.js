2e244a8306e4e8e736231b5e2093c5b1878dba72
qiu deqing <deqing.qiu@gmail.com>
2013-12-17 23:19:57 +0800

    images header

---
+++ images/js/account.js
@@ -0,0 +1,16 @@
+﻿function Account(selector) {
+  this.account = dome.selectAll(selector);
+  this.items = this.account.selectAll(".account__operation-item");
+  this.togger = this.account.selectAll(".app__list-toggle");
+  this.togger.addEventListener("click", function (event) {
+    var target = event.target;
+    var cls = target.dataset.appContainer;
+    while (target !== null && target.nodeType === 1
+      && !dome.hasClass(target, cls)) {
+      target = target.nextElementSibling;
+    } // end while
+    if (target !== null && target.nodeType === 1) {
+      dome.toggleClass(target, "app__list-container--active");
+    } // end if
+  }, false);
+} // end Account()
---
+++ images/js/images.js
@@ -0,0 +1,5 @@
+(function () {
+  dome.select(window).addEventListener("load", function (){
+    var account = new Account(".account__operation-list");
+  }, false);
+}());
