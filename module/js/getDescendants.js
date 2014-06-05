aeb4cfa46b7d691da34cf173d6f5226f2a7bfb28
deqing.qiu@gmail.com <deqing.qiu@gmail.com>
2014-06-05 15:30:12 +0800

    update

---
+++ module/js/getDescendants.js
@@ -0,0 +1,27 @@
+/**
+ * 编写一个JavasSript函数，
+ * 给定一个DOM节点node和一个正整数n,
+ * 返回node的所有第n代后代节点（直接子节点为第1代）
+ * 
+ * @param {[type]} node [node description]
+ * @param {[type]} n [n description]
+ * @return {[type]} [return description]
+ */
+function getDescendants(node, n) {
+    var p = [node];
+    var result = [];
+    var foreach = Array.prototype.forEach;
+    var i, len, cur, childNodes, child;
+
+    while (n--) {
+        result = [];
+        for (i = 0, len = p.length; i < len; ++i) {
+            foreach.call(p[i].childNodes, function (d, i) {
+              result.push(d);
+            });
+        } // end for
+        p = result;
+    } // end while
+
+    return result;
+}
